d3616ad3660b20563c861a78f891182091a78587
Jimmy Lin <linjimmy1003.md10@nycu.edu.tw>
2026-03-18 03:10:02 +0800

    Fix SHA256 checksum for public repo download

--- Casks/med-schedule-tracker.rb
+++ Casks/med-schedule-tracker.rb
@@ -1,6 +1,6 @@
 cask "med-schedule-tracker" do
   version "0.1.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "7a004cb35ee55dd205934cbd53dd937240f15539e572b800a1c46be6a8faebac"
 
   url "https://github.com/Jimmy110101013/med-schedule-backend/releases/download/v0.1.0/Med.Schedule.Tracker_0.1.0_aarch64.dmg"
   name "Med Schedule Tracker"
